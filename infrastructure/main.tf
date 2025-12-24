19e3aa81d3538dc83ef3f87e9178ebeca92b8a8f
kattyanami <amitkattyan@gmail.com>
2025-12-24 14:38:36 +0100

    fixing model container image

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -330,10 +330,10 @@ resource "aws_sagemaker_model" "anomaly_model" {
   execution_role_arn = aws_iam_role.sagemaker_role.arn
 
   primary_container {
-    image          = "683313688378.dkr.ecr.eu-west-2.amazonaws.com/sagemaker-scikit-learn:1.2-1-cpu-py3"
-    mode           = "SingleModel"
-    model_data_url = "s3://${aws_s3_bucket.models.bucket}/model/model.tar.gz"
-  }
+  image          = "683313688378.dkr.ecr.eu-west-2.amazonaws.com/sagemaker-scikit-learn:1.2-2-cpu-py3"
+  mode           = "SingleModel"
+  model_data_url = "s3://${aws_s3_bucket.models.bucket}/model/model.tar.gz"
+}
 
   tags = {
     Project = var.project_name
